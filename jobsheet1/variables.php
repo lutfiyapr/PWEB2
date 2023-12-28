755639731578b52274857637d3453031f9f93b76
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2023-12-28 17:07:43 +0700

    jobsheet1

---
+++ jobsheet1/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+    //Pendeklarasian Variabel
+    $integer=25; //contoh variabel untuk integer
+    $string="Praktikum Web 2"; // contoh variabel untuk string
+    $boolean=true; //contoh variabel untuk boolean
+
+    //output
+    echo "Ini contoh penerapan Integer $integer </br>"; //menampilkan output variabel dengan value 25 dan memberikan enter
+    echo "Ini contoh penerapan String $string </br>"; //menampilkan output variabel dengan value Praktikum Web 2 dan memberikan enter
+    echo "Ini contoh penerapan Boolean $boolean </br>";//menampilkan output variabel dengan value True dan memberikan enter
+    ?>
+</body>
+</html>
